9e97202308fcb7e4b33062df86d803db1e0d8e83
AdamMaley <adamemaley@yahoo.com>
2020-01-11 16:35:10 -0500

    Done with styling

--- starwars/src/components/card/Card.js
+++ starwars/src/components/card/Card.js
@@ -7,7 +7,19 @@ import "./Card.css";
 const CardWrapper = styled.div`
         display: flex;
         flex-direction: column;
+        justify-content: center;
+        background-color: white;
+        padding: 2%;
+        margin: 2%;
         width: 50%;
+`;
+
+const Heading = styled.h1`
+        font-size: 1.3rem;
+`;
+
+const Paragraph = styled.p`
+        margin: 1%;
 `;
 
 
@@ -20,15 +32,15 @@ function Card(props) {
 
         
         <CardWrapper>
-            <h1>Name: {props.character.name}</h1>
-            <p>Height: {props.character.height}</p>
-            <p>Mass: {props.character.mass}</p>
-            <p>Hair Color: {props.character.hair_color}</p>
-            <p>Skin Color: {props.character.skin_color}</p>
-            <p>Eye Color: {props.character.eye_color}</p>
-            <p>Birth Year: {props.character.birth_year}</p>
-            <p>Gender: {props.character.gender}</p>
-            <p>Homeworld: {props.character.homeworld}</p>
+            <Heading>Name: {props.character.name}</Heading>
+            <Paragraph>Height: {props.character.height}</Paragraph>
+            <Paragraph>Mass: {props.character.mass}</Paragraph>
+            <Paragraph>Hair Color: {props.character.hair_color}</Paragraph>
+            <Paragraph>Skin Color: {props.character.skin_color}</Paragraph>
+            <Paragraph>Eye Color: {props.character.eye_color}</Paragraph>
+            <Paragraph>Birth Year: {props.character.birth_year}</Paragraph>
+            <Paragraph>Gender: {props.character.gender}</Paragraph>
+            <Paragraph>Homeworld: {props.character.homeworld}</Paragraph>
         </CardWrapper>
     )
 
